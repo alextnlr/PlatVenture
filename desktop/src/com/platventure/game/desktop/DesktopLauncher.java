c0b426abb111384f39c9a9fe6e3a0e4354d001cb
niciavel <alexandre.tonnelier8@etu.univ-lorraine.fr>
2021-11-12 13:11:48 +0100

    Push des mains Desktop et android

---
+++ desktop/src/com/platventure/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.platventure.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.platventure.game.PlatVenture;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PlatVenture(), config);
+	}
+}
